099e5cdeacd69eb82d472ea6bb0424e0c45b31b0
Kai NeSmith <nesmithkai@gmail.com>
2025-04-02 18:05:22 -0500

    Template code

---
+++ src/prodcon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File: prodcon.py
+Author: Evan Childers, August Connors, and Kai NeSmith
+Description: A program that takes two DFAs as input and produces a new DFA via product construction.
+"""
+
+__author__ = "Evan Childers, August Connors, and Kai NeSmith"
+__credits__ = ["Evan Childers", "August Connors", "Kai NeSmith"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+
+def main():
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
